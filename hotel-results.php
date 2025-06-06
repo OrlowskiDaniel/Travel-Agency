765a2977dffa0dd691098a12889c68f36264c92c
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2025-06-06 11:53:41 +0200

    hotel-results page
    this page displays all hotels

---
+++ hotel-results.php
@@ -0,0 +1,46 @@
+<?php
+      $page_title = 'Hotel-results';
+?>
+<?php include("includes/head.php"); ?>
+</head>
+<body>
+    <?php include("includes/header.php") ?>
+    <main>
+        <section>
+            <form class="hotel-search-form" action="hotel-results.php" method="GET">
+                <div class="hotel-search-background">
+                    <input class="input-style-2 hotel-search-input" type="text" name="place" placeholder="Where are you going?">
+                    <input class="input-style-2 hotel-search-input" type="date" name="check-in">
+                    <input class="input-style-2 hotel-search-input" type="date" name="check-out">
+                    <input class="input-style-2 hotel-search-input hotel-search-person" type="number" name="person" placeholder="person" min="1" max="5">
+                    <button class="button-style-2 hotel-search-button" type="submit">Search</button>
+                </div>
+            </form>
+        </section>
+        <section class="search-results-section">
+            <h2>Recommended hotels</h2>
+            <?php include("dbcalls/search-hotel.php"); ?>
+            <?php if ($hotels): ?>
+                <?php foreach ($hotels as $hotel): ?>
+                    <div class="search-result-hotels box-style-2">
+                        <div class="hotel-foto">
+                            <img src="assets/img/<?= $hotel['hotel_img'] ?>" alt="foto-of-hotel">
+                        </div>
+                        <div class="hotel-description">
+                            <h3><?= $hotel['name'] ?> <?= $hotel['stars'] ?></h3>
+                            <p><?= $hotel['city'] ?> , <?= $hotel['country'] ?></p>
+                            <p><small><?= $hotel['description'] ?></small></p>
+                        </div>
+                        <div class="hotel-button">
+                            <a href="<?= $hotel['name'] ?>.php" class="button-style-1 show-hotel-button">Show prices</a>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <p>No hotels found.</p>
+            <?php endif; ?>
+        </section>
+    </main>
+    <?php include("includes/footer.php") ?>
+</body>
+</html>
