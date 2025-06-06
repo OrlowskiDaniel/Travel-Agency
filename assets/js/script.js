49a5924f5f199ecb8c60cd5ac14938c897247bf8
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2025-06-06 11:42:06 +0200

    header and admin-sidebar button highlight
    Javascript to highlight buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,31 @@ function hideSidebar() {
 /* learn how it works */
  function showOptions() {
     document.location.href='contact.php';
+window.addEventListener("DOMContentLoaded", function () {
+  var currentPath = window.location.pathname.split("/").pop(); // g et current file name
+  var header = document.getElementById("admin-side-bar");
+  var btns = header.getElementsByClassName("btn");
+
+  for (var i = 0; i < btns.length; i++) {
+    var btnPath = btns[i].getAttribute("href").split("/").pop();
+    if (btnPath === currentPath) {
+      btns[i].classList.add("active");
+    }
+  }
+});
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  var currentPath = window.location.pathname.split("/").pop(); // get current file name
+  var header = document.getElementById("header");
+  var btns = header.getElementsByClassName("header-btn");
+
+  for (var i = 0; i < btns.length; i++) {
+    var btnPath = btns[i].getAttribute("href").split("/").pop();
+    if (btnPath === currentPath) {
+      btns[i].classList.add("active");
+    }
+  }
+});
+
  }
--- includes/admin-sidebar.php
+++ includes/admin-sidebar.php
@@ -1,14 +1,14 @@
-<div class="admin-side-bar">
+<div id="admin-side-bar">
     <div class="admin-user-box">
         <p>Username</p>
     </div>
     <ul>
-        <li><a href="./admin.php" class="blue-hover">Dashboard</a></li>
-        <li><a href="./admin-users.php" class="blue-hover">Users</a></li>
-        <li><a href="./admin-bookings.php" class="blue-hover">Booking</a></li>
-        <li><a href="./admin-messages.php" class="blue-hover">Messages</a></li>
-        <li><a href="./admin-vlight.php" class="blue-hover">Edit Vlights</a></li>
-        <li><a href="./admin-hotel.php" class="blue-hover">Edit Hotels</a></li>
+        <li><a href="./admin.php" class="btn blue-hover">Dashboard</a></li>
+        <li><a href="./admin-users.php" class="btn blue-hover">Users</a></li>
+        <li><a href="./admin-bookings.php" class="btn blue-hover">Booking</a></li>
+        <li><a href="./admin-messages.php" class="btn blue-hover">Messages</a></li>
+        <li><a href="./admin-edit-flights.php" class="btn blue-hover">Edit Vlights</a></li>
+        <li><a href="./admin-edit-hotels.php" class="btn blue-hover">Edit Hotels</a></li>
 
         <form action="./dbcalls/process.php" method="post">
             <input type="submit" class="admin-log-out-button gray-hover" value="Log Out">
